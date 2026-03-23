c389e032af77419cfade10a47fc99459c8f5dd4a
luca2453 <luca.c2453@gmail.com>
2026-03-23 14:12:52 -0300

    'List' changed to 'list'

--- Easy/0001-two-sum/solution.py
+++ Easy/0001-two-sum/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
